9570e9a736434669240ff53efa2796a61b11a793
EBJ <eldhobabu376@gmail.com>
2023-04-08 14:33:36 +0530

    added __init__ function for the first class

--- ai.py
+++ ai.py
@@ -11,3 +11,20 @@ from gym.wrappers import SkipWrapper
 from ppaquette_gym_doom.wrappers.action_space import ToDiscrete
 import experience_replay, image_preprocessing
 
+#making the brain
+
+class CNN(nn.Module):
+    
+    def __init__(self, number_actions):
+        super(CNN, self).__init__()
+        self.convolution1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5)
+        self.convolution2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3)
+        self.convolution3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 2)
+        self.fc1 = nn.Linear(in_features = number_nuerons, out_features = 40)
+        self.fc2  =  nn.Linear(in_features = 40, out_features = number_actions)
+        
+    
+        
+        
+        
+
